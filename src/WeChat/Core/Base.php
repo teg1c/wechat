2042548cf766d526800722c6faf9a6a7e1451f92
tegic <teg1c@foxmail.com>
2020-09-06 00:04:19 +0800

    1.0.0 version

---
+++ src/WeChat/Core/Base.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by tegic. Date: 2020/09/06 Time: 17:19
+ */
+
+namespace WeChat\Core;
+
+/**
+ * Class WxBase 抽象公用静态方法类
+ * @package wechat
+ */
+abstract class Base
+{
+    use \WeChat\Extend\Tool;
+
+
+}
